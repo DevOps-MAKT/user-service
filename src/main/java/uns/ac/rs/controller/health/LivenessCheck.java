d87dffc2faf8d06854f4cea80a2dbb29168f6ebe
KatarinaKomad <komad.katarina@gmail.com>
2024-06-12 17:58:20 +0200

    feat: Add liveness and readiness probe endpoint

---
+++ src/main/java/uns/ac/rs/controller/health/LivenessCheck.java
@@ -0,0 +1,15 @@
+package uns.ac.rs.controller.health;
+import jakarta.enterprise.context.ApplicationScoped;
+import org.eclipse.microprofile.health.HealthCheck;
+import org.eclipse.microprofile.health.HealthCheckResponse;
+import org.eclipse.microprofile.health.Liveness;
+
+@Liveness
+@ApplicationScoped
+public class LivenessCheck implements HealthCheck {
+
+    @Override
+    public HealthCheckResponse call() {
+        return HealthCheckResponse.up("Liveness check");
+    }
+}
